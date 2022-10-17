ffec71256b91400cc4be479e77cc89708aa2864f
Paulus Triswandani <devishtank@gmail.com>
2022-10-17 17:57:56 +0700

    A bunch of code dropped but still sometimes error on generating random number

--- src/Game.kt
+++ src/Game.kt
@@ -1,3 +1,41 @@
-fun main(args: Array<String>){
+import kotlin.random.Random
+
+fun main (args: Array<String>){
 	val options = arrayOf("Rock","Paper","Scissors")
+	val gameChoice = getGameChoice(options)
+	val userChoice = getUserChoice(options)
+	printResult(userChoice,gameChoice)
+}
+
+fun getGameChoice(optionsParam: Array<String>) = optionsParam[Random.nextInt() % optionsParam.size]
+
+fun getUserChoice(optionsParam: Array<String>) : String {
+	var isValidChoice = false
+	var userChoice = ""
+	while(!isValidChoice){
+		print("Please enter one of the following:")
+		for (item in optionsParam) print(" $item ")
+		println(".")
+		val userInput = readLine()
+		if (userInput != null && userInput in optionsParam){
+			isValidChoice = true
+			userChoice = userInput
+		}
+		//if the choice is invalid, inform user
+		if(!isValidChoice) println("You must enter a valid choice")
+	}
+	return userChoice
+}
+
+fun printResult(userChoice: String, gameChoice: String){
+	val result: String
+
+	if (userChoice==gameChoice) result = "Tie!"
+	else if ((userChoice == "Rock") && (gameChoice == "Scissors") ||
+		(userChoice == "Paper") && (gameChoice == "Rock") ||
+		(userChoice == "Scissors") && (gameChoice == "Paper")
+	) result = "You Win!"
+	else result = "You Lose"
+
+	println("You chose $userChoice. I chose $gameChoice. $result")
 }
